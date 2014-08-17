1bc9839de929829739b6db1c74400c1a42ec2588
Aubergine Solutions <hello@auberginesolutions.com>
2014-08-17 08:51:48 +0530

    commit

--- test/test.js
+++ test/test.js
@@ -19,7 +19,7 @@ describe('titleCtrl', function()
 	{
 		expect($scope.title).toBe("Hello Friends!");
 	});
-	it('By Clicking the button changes the title', function()
+	it('Clicking the button changes the title', function()
 	{
 		$scope.changeIt();
 		expect($scope.title).toBe("How are you?");
